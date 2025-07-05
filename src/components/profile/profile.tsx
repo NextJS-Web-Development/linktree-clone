39f274ee5640516a4a00cb9ab34820131fc1044d
Hafemi <pascal.hafemi@gmail.com>
2025-07-05 19:42:20 +0200

    feat: finish styling

--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 export default function ProfileComponent(): JSX.Element {
   return (
-    <div>
+    <div className={styles.profileContainer}>
       <Image src="/PFP-Eagle.jpg" height="100" width="100" alt="Profile Picture" className={styles.profilePicture} />
       <p className={styles.profileName}>Hafemi</p>
       <p className={styles.profileBio}>Fischers Fritz fischt frische Fische.</p>
