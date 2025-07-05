4aeacf35dfe8973e0fe348d965512513c7e73856
Hafemi <pascal.hafemi@gmail.com>
2025-07-05 18:54:17 +0200

    feat: adjust profile size

--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 export default function ProfileComponent(): JSX.Element {
   return (
     <div>
-      <Image src="/PFP-Eagle.jpg" height="150" width="150" alt="Profile Picture" className={styles.profilePicture} />
+      <Image src="/PFP-Eagle.jpg" height="100" width="100" alt="Profile Picture" className={styles.profilePicture} />
       <p className={styles.profileName}>Hafemi</p>
       <p className={styles.profileBio}>Fischers Fritz fischt frische Fische.</p>
     </div>
