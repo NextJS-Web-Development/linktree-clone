d60649eea9f8621f2ab8ef39b1e0fc7d2638de40
Hafemi <pascal.hafemi@gmail.com>
2025-07-05 18:52:22 +0200

    feat: add profile information

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import styles from "./page.module.css";
+import ProfileComponent from "@/components/profile/profile";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <ProfileComponent/>
     </div>
   );
 }
---
+++ src/components/profile/profile.tsx
@@ -0,0 +1,13 @@
+import styles from "./profile.module.css";
+import { JSX } from "react";
+import Image from 'next/image';
+
+export default function ProfileComponent(): JSX.Element {
+  return (
+    <div>
+      <Image src="/PFP-Eagle.jpg" height="150" width="150" alt="Profile Picture" className={styles.profilePicture} />
+      <p className={styles.profileName}>Hafemi</p>
+      <p className={styles.profileBio}>Fischers Fritz fischt frische Fische.</p>
+    </div>
+  );
+}
